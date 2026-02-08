a7489372ac140ab2d1853aaa7d364d18c7060468
Stalmi <11790996+stalmi@users.noreply.github.com>
2026-02-08 23:50:11 +0100

    Aliases , git, nano

--- post-alias.sh
+++ post-alias.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
-# Definicja zmian, które chcemy dodać
+#----------
+# GIT
+# Tworzy czytelny graf logów pod komendą 'git lg'
+git config --global alias.lg "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+# Podstawowe aliasy dla szybkości
+git config --global alias.s "status -s"
+git config --global alias.co "checkout"
+git config --global alias.br "branch"
+
+# Włączenie kolorów w interfejsie CLI
+git config --global color.ui auto
+
+#----------
+# Nano konfiguracja
+nano_conf="$HOME/.nanorc"
+
+# Konfiguracja: 2 spacje zamiast Tab + podświetlanie składni
+# Wczytujemy wszystkie dostępne schematy kolorów z /usr/share/nano/
+config_payload=$(cat << 'EOF'
+#custom config start
+set tabsize 2
+set tabstospaces
+#set whitespace "»·"
+set whitespace "» "
+set linenumbers
+set autoindent
+set constantshow
+include "/usr/share/nano/*.nanorc"
+EOF
+)
+
+if [ ! -f "$nano_conf" ]; then
+  echo -e "$config_payload" > "$nano_conf"
+  echo -e "\e[32m[ok]\e[0m Set a new config $nano_conf"
+elif ! grep -q "custom config start" "$nano_conf"; then
+  echo -e "$config_payload" >> "$nano_conf"
+  echo -e "\e[32m[ok]\e[0m Updated nano (taby, kolory dla Python/Java/PHP/Bash/SQL)"
+else
+	echo -e "\e[33m[skip]\e[0m $nano_conf posiada już konfigurację"
+fi
+
+#----------
+# Aliases Definicja zmian, które chcemy dodać
 CONFIG_FILE="$HOME/.aliases"
 
 EXTRA_CONFIG=$(cat << 'EOF'
@@ -49,6 +92,17 @@ update_config() {
             echo -e "\e[33m[SKIP]\e[0m $FILE posiada już konfigurację"
         fi
     fi
+if [ ! -f "$FILE" ]; then
+    echo -e "$extra_config" > "$FILE"
+    echo -e "\e[32m[ok]\e[0m utworzono nowy plik $FILE"
+elif ! grep -q "custom config start" "$FILE"; then
+    echo -e "$extra_config" >> "$FILE"
+    echo -e "\e[32m[ok]\e[0m zaktualizowano $FILE"
+else
+    echo -e "\e[33m[skip]\e[0m $FILE posiada już konfigurację"
+fi
+
+
 }
 #Run
 update_config "$CONFIG_FILE"
